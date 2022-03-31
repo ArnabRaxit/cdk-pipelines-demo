72212578bcaec16ef124a926c416241d27ace41d
Arnab Rakshit <arnab.rakshit@aol.com>
2022-03-31 02:46:17 +0000

    modified for inno account

--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import pipelines
 
 from .webservice_stage import WebServiceStage
 
-APP_ACCOUNT = '123456789012'
+APP_ACCOUNT = '472009282539'
 
 class PipelineStack(core.Stack):
   def __init__(self, scope: core.Construct, id: str, **kwargs):
@@ -22,8 +22,8 @@ class PipelineStack(core.Stack):
         action_name='GitHub',
         output=source_artifact,
         oauth_token=core.SecretValue.secrets_manager('github-token'),
-        owner='OWNER**REPLACEME',
-        repo='REPO**REPLACEME',
+        owner='ArnabRaxit',
+        repo='cdk-pipelines-demo',
         trigger=cpactions.GitHubTrigger.POLL),
 
       synth_action=pipelines.SimpleSynthAction(
@@ -33,27 +33,27 @@ class PipelineStack(core.Stack):
         build_command='pytest unittests',
         synth_command='cdk synth'))
 
-    pre_prod_app = WebServiceStage(self, 'Pre-Prod', env={
-      'account': APP_ACCOUNT,
-      'region': 'eu-central-1',
-    })
-    pre_prod_stage = pipeline.add_application_stage(pre_prod_app)
-    pre_prod_stage.add_actions(pipelines.ShellScriptAction(
-      action_name='Integ',
-      run_order=pre_prod_stage.next_sequential_run_order(),
-      additional_artifacts=[source_artifact],
-      commands=[
-        'pip install -r requirements.txt',
-        'pytest integtests',
-      ],
-      use_outputs={
-        'SERVICE_URL': pipeline.stack_output(pre_prod_app.url_output)
-      }))
+    # pre_prod_app = WebServiceStage(self, 'Pre-Prod', env={
+    #   'account': APP_ACCOUNT,
+    #   'region': 'ap-southeast-2',
+    # })
+    # pre_prod_stage = pipeline.add_application_stage(pre_prod_app)
+    # pre_prod_stage.add_actions(pipelines.ShellScriptAction(
+    #   action_name='Integ',
+    #   run_order=pre_prod_stage.next_sequential_run_order(),
+    #   additional_artifacts=[source_artifact],
+    #   commands=[
+    #     'pip install -r requirements.txt',
+    #     'pytest integtests',
+    #   ],
+    #   use_outputs={
+    #     'SERVICE_URL': pipeline.stack_output(pre_prod_app.url_output)
+    #   }))
 
-    pipeline.add_application_stage(WebServiceStage(self, 'Prod', env={
-      'account': APP_ACCOUNT,
-      'region': 'eu-central-1',
-    }))
+    # pipeline.add_application_stage(WebServiceStage(self, 'Prod', env={
+    #   'account': APP_ACCOUNT,
+    #   'region': 'ap-southeast-2',
+    # }))
 
 
 
